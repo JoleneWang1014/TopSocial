54328a8424476b07dc6dfa406d42bef1a90f074d
jolene <jolene@au2-sm-dt-mkt02.local>
2025-02-21 17:57:34 +1100

    Updated 02212025-V3

--- backend/routes/userProfiles.js
+++ backend/routes/userProfiles.js
@@ -1,32 +1,11 @@
 const express = require('express');
-const axios = require('axios');
+
+const { getUserProfileById } = require('../controllers/userProfilesController');
+
 const userProfilesRouter = express.Router();
 
 
-const USER_PROFILES_API_URL = 'http://localhost:8000/userProfiles';
-
 // GET get UserProfile by id /api/userProfiles/:userId
-userProfilesRouter.get('/:userId', async (req, res) => {
-    console.log('Received userId:', req.params.userId);
-
-    const id = req.params.userId;
-
-    if(!id){
-        return res.status(400).json({ msg: 'User ID is required' });
-    }
-    try {
-        const response = await axios.get(`${USER_PROFILES_API_URL}/${id}`);
-
-        const userProfile = response.data;
-
-        res.status(200).json({
-            msg:'Get userProfile successfully',
-            data: userProfile   
-        });
-    } catch (error) {
-        console.error(error);
-    }
-}
-);  
+userProfilesRouter.get('/:userId', getUserProfileById);  
 
 module.exports = userProfilesRouter;
